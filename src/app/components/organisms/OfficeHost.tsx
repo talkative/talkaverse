5b96a22cf23f94e75fcd6c81e4878f075101a82f
Clara Isaksson <clara@talkative.se>
2023-11-22 16:57:15 +0100

    🚧 first draft of office hosts and theme component

--- src/app/components/organisms/OfficeHost.tsx
+++ src/app/components/organisms/OfficeHost.tsx
@@ -1,7 +1,34 @@
+import LisaÖrtlund from '../../../../public/image/lisa.svg';
+import Image from 'next/image';
+
 const OfficeHost: any = () => {
+  const firstSelectedOfficeHost = 'Lisa Örtlund';
+  const secondSelectedOfficeHost = 'Lisa Örtlund';
+  const officeHosts = `${firstSelectedOfficeHost} and ${secondSelectedOfficeHost}`;
+  const theme = 'chips';
+
   return (
-    <div>
-      <p className="border-2 border-rose-500">Office hosts + Theme</p>
+    <div className="w-1/3 space-y-6 border-2 border-rose-500">
+      <div className="flex flex-col text-center">
+        <span className="font-bold">Office Hosts:</span>
+        <span className="">{officeHosts}</span>
+      </div>
+      <div className="flex justify-center h-32">
+        <div className="flex w-1/2">
+          <Image
+            src={LisaÖrtlund}
+            alt="illustration of an employee at Talkative"
+          />
+          <Image
+            src={LisaÖrtlund}
+            alt="illustration of an employee at Talkative"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col text-center">
+        <span className="font-bold">Theme: </span>
+        <span className="">{theme}</span>
+      </div>
     </div>
   );
 };
