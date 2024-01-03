bb3ea1bdb934d945de79afac841aa4757a9610d5
b3cka <rebecka.warulf@gmail.com>
2024-01-03 19:11:41 +0100

    feat(office host): minor adjustments

--- src/app/components/organisms/OfficeHost.tsx
+++ src/app/components/organisms/OfficeHost.tsx
@@ -4,30 +4,29 @@ import Image from 'next/image';
 const OfficeHost: any = () => {
   const firstSelectedOfficeHost = 'Lisa Örtlund';
   const secondSelectedOfficeHost = 'Lisa Örtlund';
-  const officeHosts = `${firstSelectedOfficeHost} and ${secondSelectedOfficeHost}`;
   const theme = 'chips';
 
   return (
-    <div className="w-1/3 space-y-6 border-2 border-rose-500">
-      <div className="flex flex-col text-center">
-        <span className="font-bold">Office Hosts:</span>
-        <span className="">{officeHosts}</span>
+    <div className="p-4 space-y-6">
+      <div className="flex flex-col text-end">
+        <span className="text-2xl font-bold">Office Hosts</span>
+        <span>{`${firstSelectedOfficeHost} & ${secondSelectedOfficeHost}`}</span>
       </div>
-      <div className="flex justify-center h-32">
-        <div className="flex w-1/2">
-          <Image
-            src={LisaÖrtlund}
-            alt="illustration of an employee at Talkative"
-          />
-          <Image
-            src={LisaÖrtlund}
-            alt="illustration of an employee at Talkative"
-          />
-        </div>
+      <div className="inline-flex content-end h-32">
+        <Image
+          src={LisaÖrtlund}
+          alt="illustration of an employee at Talkative"
+          className="z-0 w-full h-full aspect-[1/1] rounded overflow-hidden relative top-6 left-4"
+        />
+        <Image
+          src={LisaÖrtlund}
+          alt="illustration of an employee at Talkative"
+          className="z-10 w-full h-full aspect-[1/1] rounded overflow-hidden relative"
+        />
       </div>
-      <div className="flex flex-col text-center">
-        <span className="font-bold">Theme: </span>
-        <span className="">{theme}</span>
+      <div className="flex flex-col text-start">
+        <span className="text-xl font-bold">Theme</span>
+        <span>{theme}</span>
       </div>
     </div>
   );
