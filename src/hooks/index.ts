f55e3890b5fc7677002cd1197bd392eda3d5840d
b3cka <rebecka.warulf@gmail.com>
2024-02-28 16:23:34 +0100

    added athentication

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,4 +2,8 @@ const axios = require('axios').default;
 
 export const RESTClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  auth: {
+    username: process.env.NEXT_PUBLIC_HTTP_BASIC_USER,
+    password: process.env.NEXT_PUBLIC_HTTP_BASIC_PASS,
+  },
 });
