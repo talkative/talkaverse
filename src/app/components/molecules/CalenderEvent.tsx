58e6e78d0d174792267029898328b708b87c5549
Maria Lundhammar <marialundhammar@gmail.com>
2024-02-28 16:36:04 +0100

    using data from google calendar

--- src/app/components/molecules/CalenderEvent.tsx
+++ src/app/components/molecules/CalenderEvent.tsx
@@ -2,19 +2,85 @@ interface CalendarEventType {
   title?: string;
   date?: string;
   time?: number;
+  data: any;
 }
 
-const CalendarEvent: React.FC<CalendarEventType> = ({ title, date, time }) => {
-  console.log(title, date);
+const CalendarEvent: React.FC<CalendarEventType> = ({
+  title,
+  date,
+  time,
+  data,
+}) => {
+  console.log('hajjj', data);
+
+  const eventTimestampStart = data.start.dateTime;
+  const eventTimestampEnd = data.end.dateTime;
+  const eventStart = new Date(eventTimestampStart);
+  const eventEnd = new Date(eventTimestampEnd);
+
+  function getHours(event: string) {
+    const hours = new Date(event).getHours();
+    return hours;
+  }
+
+  function getMinutes(event: string) {
+    const minutes = new Date(event).getMinutes();
+    return minutes;
+  }
+  function getDate(parsedTimeStamp: string) {
+    const date = eventStart.getMinutes();
+    return date;
+  }
+
+  function getDay(parsedTimeStamp: string) {
+    const day = eventStart.getDay();
+    const dayNames = [
+      'Söndag',
+      'Måndag',
+      'Tisdag',
+      'Onsdag',
+      'Torsdag',
+      'Fredag',
+      'Lördag',
+    ];
+    return dayNames[day];
+  }
+
+  console.log('data.end.dateTime', getHours(data.end.dateTime));
 
   return (
-    <div className=" mx-2 text-black p-4 space-x-6 space-y-4 rounded-lg bg-almostwhite h-full p-4 flex flex-row">
-      <div className="flex w-1/3 mr-2">
-        <p className="font-raleway mt-4 text-5xl font-bold"> {`${date}`} </p>
+    <div className="border-2 text-black">
+      <div className="text-2xl">
+        {eventStart && (
+          <>
+            <p>{`${eventStart.getDate()} / ${eventStart.getMonth() + 1}`}</p>
+          </>
+        )}
       </div>
-      <div className="text-xl ">
-        <p className="font-raleway font-bold "> {`  ${title} `} </p>
-        {time && <p>{time}</p>}
+      <div className="text-xl">
+        <p className="font-raleway font-bold "> {`  ${data.summary} `} </p>
+        {data.start.dateTime && (
+          <>
+            <div className="flex flex-row">
+              <p>{getHours(data.start.dateTime)}:</p>
+              <p> {getMinutes(data.start.dateTime)}0</p>
+            </div>
+          </>
+        )}
+        {data.end.dateTime && (
+          <>
+            <div className="flex flex-row text-red-400">
+              <p>{getHours(data.end.dateTime)}:</p>
+              <p> {getMinutes(data.end.dateTime)}0</p>
+            </div>
+            <div>
+              <p>{getDay(data.end.dateTime)}</p>
+            </div>
+          </>
+        )}
+
+        {data.start.date && <p>{data.start.date}</p>}
+        {data.start.date && <p>{getDate(data.start.dateTime)}</p>}
       </div>
     </div>
   );
--- src/app/components/organisms/Calendar.tsx
+++ src/app/components/organisms/Calendar.tsx
@@ -4,28 +4,23 @@ import CalendarEvent from '../molecules/CalenderEvent';
 
 import { useGetCalendarEvents } from '@/hooks/useCalendar';
 
-console.log('events', events);
-
 const Calendar: any = () => {
-  console.log(events);
-
   const { data, status } = useGetCalendarEvents();
 
-  console.log({ data, status });
+  if (!data) {
+    return null;
+  }
 
   return (
-    <div className="flex flex-col justify-between h-full">
-      {events.map((event, index) => (
-        <>
-          <div className="h-200">
-            <CalendarEvent
-              date={event.date}
-              title={event.title}
-              time={'kl 17.00'}
-            />
-          </div>
-        </>
-      ))}
+    <div className="flex flex-col space-y-4 my-2 overflow-hidden justify-between h-full">
+      {data &&
+        data.data.slice(0, 3).map((data, index) => (
+          <>
+            <div className="h-200">
+              <CalendarEvent title={data.summary} data={data} />
+            </div>
+          </>
+        ))}
     </div>
   );
 };
